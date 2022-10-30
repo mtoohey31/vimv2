e6fb2dd5aa0af2454278d225915d15aabe2d433b
Matthew Toohey <contact@mtoohey.com>
2022-10-30 19:51:27 -0400

    build: update flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,28 @@
 {
   description = "vimv2";
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-  };
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        with import nixpkgs { inherit system; }; rec {
-          packages.vimv2 = buildGo118Module rec {
-            name = "vimv2";
-            pname = name;
-            src = ./.;
-            vendorSha256 = "jlREqPL8nF82i8iD2TJ7qNCZUXgeNQgN2EShpruVuGc=";
-          };
-          defaultPackage = packages.vimv2;
 
-          devShell = mkShell {
-            nativeBuildInputs = [ go_1_18 gopls ];
-          };
-        }) // {
-      overlay = (final: prev: { vimv2 = self.defaultPackage."${prev.system}"; });
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: {
+    overlays.default = final: _: {
+      vimv2 = final.buildGoModule rec {
+        name = "vimv2";
+        pname = name;
+        src = ./.;
+        vendorSha256 = null;
+      };
     };
+  } // utils.lib.eachDefaultSystem (system: with import nixpkgs
+    {
+      overlays = [ self.overlays.default ]; inherit system;
+    }; {
+    packages.default = vimv2;
+
+    devShells.default = mkShell {
+      packages = [ go gopls revive ];
+    };
+  });
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module mtoohey.com/vimv2
 
-go 1.18
+go 1.19
 
 require github.com/alecthomas/kong v0.6.1
