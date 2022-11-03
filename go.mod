2812ce3e4b200c91f117fa8e419d60d62bb75a77
Matthew Toohey <contact@mtoohey.com>
2022-11-03 00:05:34 -0400

    feat: prompt for edit on malformed input

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module mtoohey.com/vimv2
 
 go 1.19
 
-require github.com/alecthomas/kong v0.6.1
+require (
+	github.com/alecthomas/kong v0.6.1
+	golang.org/x/term v0.1.0
+)
+
+require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
--- main.go
+++ main.go
@@ -4,15 +4,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"runtime"
 
 	"github.com/alecthomas/kong"
+	"golang.org/x/term"
 )
 
-// TODO: prompt users on whether they want to retry (with the tmpfile reset or
-// the same) if they enter invalid stuff.
+// TODO: remove dependencies
 
 var cli struct {
 	Directory string `arg:"" default:"." type:"existingdir" help:"The directory in which you want to rename files."`
@@ -25,10 +26,13 @@ func main() {
 	exitCode := 0
 	defer func() { os.Exit(exitCode) }()
 
-	die := func(format string, a ...any) {
+	warn := func(format string, a ...any) {
 		fmt.Fprintf(os.Stderr, "%s: ", os.Args[0])
 		fmt.Fprintf(os.Stderr, format, a...)
 		fmt.Fprintln(os.Stderr)
+	}
+	die := func(format string, a ...any) {
+		warn(format, a...)
 		exitCode = 1
 		// we use this instead of os.Exit so that we can run all cleanup that's
 		// been deferred up 'till this point
@@ -42,16 +46,6 @@ func main() {
 		die(fmt.Sprintf("%s: %%s", format), append(a, err.Error())...)
 	}
 
-	// creating tmpfile and defering cleanup
-
-	tmpfile, err := os.CreateTemp("", "vimv2")
-	dieWrap(err, "creating tmpfile failed")
-
-	defer func() {
-		dieWrap(tmpfile.Close(), "closing tmpfile failed")
-		dieWrap(os.Remove(tmpfile.Name()), "removing tmpfile failed")
-	}()
-
 	// detecting editor
 
 	editor, editorFound := os.LookupEnv("EDITOR")
@@ -62,7 +56,10 @@ func main() {
 		die("no viable editor found, please set $EDITOR or $VISUAL")
 	}
 
-	// reading sources and writing to tmpfile
+	// toctou is inevitable, we assume that nobody touches the files from the
+	// time we read them until we exit
+
+	// reading srcs
 
 	entries, err := os.ReadDir(cli.Directory)
 	dieWrap(err, "reading directory failed")
@@ -70,49 +67,135 @@ func main() {
 	srcs := make([]string, len(entries))
 	for i, entry := range entries {
 		srcs[i] = entry.Name()
-		_, err := tmpfile.Write([]byte(entry.Name()))
-		dieWrap(err, "writing to tmpfile failed")
-		_, err = tmpfile.Write([]byte{byte('\n')})
-		dieWrap(err, "writing to tmpfile failed")
 	}
 
-	// running editor
+	// variable setup for the loop below
+	var tmpfile *os.File
+	var tmpfileCreated bool
 
-	cmd := exec.Command(editor, tmpfile.Name())
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	defer func() {
+		// cleans up the last remaining tmpfile, if one exists
+		if tmpfileCreated {
+			dieWrap(tmpfile.Close(), "closing tmpfile failed")
+			dieWrap(os.Remove(tmpfile.Name()), "removing tmpfile failed")
+		}
+	}()
 
-	dieWrap(cmd.Run(), "running editor command failed")
+	// maps for moving things
+	var srcToDst map[string]string
+	var dstSet map[string]struct{}
 
-	// reading the result of the edit and destination collision detection and
-	// data structure setup. collisions detected here cannot be resolved
-	// (without overwriting files) and will have to be revised before
-	// continuing
+	// main input loop which continues until the user enters valid input or
+	// exits intentionally
 
-	tmpfile.Seek(0, 0)
-	scanner := bufio.NewScanner(tmpfile)
+	for {
+		// creating next tmpfile, if necessary
 
-	dstSet := map[string]struct{}{}
-	srcToDst := map[string]string{}
+		if tmpfile == nil {
+			tmpfile, err = os.CreateTemp("", "vimv2")
+			dieWrap(err, "creating tmpfile failed")
+			tmpfileCreated = true
 
-	for scanner.Scan() {
-		if len(dstSet) > len(srcs) {
-			die("tmpfile contains too many lines")
+			for _, src := range srcs {
+				_, err := tmpfile.Write([]byte(src))
+				dieWrap(err, "writing to tmpfile failed")
+				_, err = tmpfile.Write([]byte{byte('\n')})
+				dieWrap(err, "writing to tmpfile failed")
+			}
+
+			dieWrap(tmpfile.Sync(), "syncing tmpfile failed")
 		}
 
-		dst := scanner.Text()
-		_, found := dstSet[dst]
-		if found {
-			die("duplicate destination \"%s\"", dst)
-		}
-		srcToDst[srcs[len(dstSet)]] = dst
-		dstSet[dst] = struct{}{}
-	}
-	dieWrap(scanner.Err(), "reading tmpfile failed")
+		// running editor
 
-	if len(dstSet) < len(srcs) {
-		die("tmpfile contains too few lines")
+		cmd := exec.Command(editor, tmpfile.Name())
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		dieWrap(cmd.Run(), "running editor command failed")
+
+		// reading the result of the edit, and validating as we go
+
+		// intialize maps
+		srcToDst = map[string]string{}
+		dstSet = map[string]struct{}{}
+
+		_, err = tmpfile.Seek(0, 0)
+		dieWrap(err, "seeking tmpfile failed")
+		scanner := bufio.NewScanner(tmpfile)
+
+		// indicates we exited the loop manually
+		inputInvalid := false
+
+		for scanner.Scan() {
+			if len(dstSet) >= len(srcs) {
+				warn("tmpfile contains too many lines")
+				inputInvalid = true
+				break
+			}
+
+			dst := scanner.Text()
+			_, found := dstSet[dst]
+			if found {
+				warn("duplicate destination \"%s\"", dst)
+				inputInvalid = true
+				break
+			}
+			srcToDst[srcs[len(dstSet)]] = dst
+			dstSet[dst] = struct{}{}
+		}
+		// if this is set, we don't need to check this because we already have
+		// an error that we're going to warn about
+		if !inputInvalid {
+			dieWrap(scanner.Err(), "reading tmpfile failed")
+
+			if len(dstSet) == len(srcs) {
+				// everything's ok, so we can continue to moving
+				break
+			}
+
+			// it can't contain too many because that would've caused an error
+			// above, steting inputInvalid
+			warn("tmpfile contains too few lines")
+		}
+
+		for {
+			// print prompt
+			fmt.Printf("[\033[1;31me\033[0mdit existing/edit " +
+				"\033[1;31mn\033[0mew/\033[1;31mq\033[0muit]: ")
+
+			// read 1 byte in raw mode so no enter is required
+			var b [1]byte
+			oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+			dieWrap(err, "failed to set terminal to raw mode")
+			_, err = os.Stdin.Read(b[:])
+			dieWrap(term.Restore(int(os.Stdin.Fd()), oldState),
+				"failed to restore terminal state")
+
+			// print char (would be nice to just use terminal echo, but that's
+			// not an option with x/term), and print newline so things show up
+			// on the next line
+			fmt.Fprintf(os.Stderr, "%c\n", b[0])
+			if err == io.EOF {
+				die("user exited")
+			}
+			dieWrap(err, "failed to read from stdin")
+
+			// proceed according to user input
+			switch b[0] {
+			case 'n', 'N':
+				dieWrap(tmpfile.Close(), "closing tmpfile failed")
+				tmpfile = nil
+				fallthrough
+			case 'e', 'E':
+				break
+			case 3 /* ^C */, 4 /* ^D */, 'q', 'Q':
+				die("user exited")
+			default:
+				warn("invalid selection '%c'", b[0])
+			}
+		}
 	}
 
 	// movement
